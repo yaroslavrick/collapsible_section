29817dcd5875b0429d9dcf7072514d7144a27c8e
Yaroslav Yenkala <yaroslav_yenkala@epam.com>
2025-07-20 16:08:52 +0300

    Rubocop happy

--- lib/collapsible_section/helper.rb
+++ lib/collapsible_section/helper.rb
@@ -3,9 +3,9 @@
 module CollapsibleSection
   module Helper
     def collapsible_section(title, open: true, &block)
-      content_tag(:details, class: 'section-collapsable', open: open) do
-        concat content_tag(:summary, class: 'section-header') do
-          content_tag(:h3, title, class: 'section-title')
+      content_tag(:details, class: "section-collapsable", open: open) do
+        concat content_tag(:summary, class: "section-header") do
+          content_tag(:h3, title, class: "section-title")
         end
         concat capture(&block) if block_given?
       end
--- lib/collapsible_section/railtie.rb
+++ lib/collapsible_section/railtie.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 module CollapsibleSection
   class Railtie < Rails::Railtie
-    initializer 'collapsible_section.helper' do
-      ActionView::Base.send :include, CollapsibleSection::Helper
+    initializer "collapsible_section.helper" do
+      ActionView::Base.include CollapsibleSection::Helper
     end
   end
 end
